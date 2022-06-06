82a19efa59bbacac550b04be359ea289ede3c22d
valiantlynx <gormerykombo@gmail.com>
2022-06-06 04:58:48 +0200

    level 3 security, hashing very hard to get back the passwords but instead just compare the hashes

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 
 const app = express();
@@ -21,7 +20,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 //home
@@ -38,7 +36,7 @@ app.route('/login')
     })
     .post(function(req, res) {
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
         User.findOne({ email: username }, function(err, foundUser) {
             if (err) {
                 console.log(err);
@@ -65,7 +63,7 @@ app.route('/register')
     .post(function(req, res) {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
         newUser.save(function(err) {
             if (err) {
