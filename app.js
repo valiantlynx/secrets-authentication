8c0cff6b7f7b39d873ba65563611a9a4614c96ba
valiantlynx <gormerykombo@gmail.com>
2022-06-06 04:27:28 +0200

    add the dotenv file to keep my secrets secure and gitignore to prevent me from uploading my secrets to  the internet

---
+++ app.js
@@ -0,0 +1,83 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+//home
+app.route('/')
+    .get(function(req, res) {
+        res.render("home");
+    });
+
+
+//login
+app.route('/login')
+    .get(function(req, res) {
+        res.render("login");
+    })
+    .post(function(req, res) {
+        const username = req.body.username;
+        const password = req.body.password;
+        User.findOne({ email: username }, function(err, foundUser) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render("secrets");
+                    } else {
+                        res.send("<h1>Failed to log in</h1>");
+                    }
+                } else {
+                    res.send("<h1>User not registered</h1>");
+                }
+            }
+        })
+
+    });
+
+//register
+app.route('/register')
+    .get(function(req, res) {
+        res.render("register");
+    })
+    .post(function(req, res) {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save(function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+    });
+
+
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
+});
